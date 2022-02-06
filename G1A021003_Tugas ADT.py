96efd2f1c9b7b63151f9bcf0ce8abc19f802a6a8
Dwita Meiriska <99007040+DwitaMeiriska02@users.noreply.github.com>
2022-02-06 19:36:23 +0700

    Update G1A021003_Tugas ADT.py

--- G1A021003_Tugas ADT.py
+++ G1A021003_Tugas ADT.py
@@ -2,3 +2,13 @@ Panjang=input("Panjang Persegi = ")
 Lebar=input('Lebar Persegi= ')
 Tinggi=input('Tinggi Persegi =')
 print("Luas Persegi Panjang", int(Lebar)*int(Panjang)*int(Tinggi))
+
+#Assalamualaikum Warahmatullahi Wabarakatuh
+#Perkenalkan Saya Dwita Meiriska (G1A021003)
+#Disini saya membuat ADT Sederhana berupa Luas Persegi panjang
+Panjang #merupakan perintah untuk memanggil inputan
+Lebar #merupakan perintah untuk memanggil inputan
+Tinggi #merupakan perintah untuk memanggil inputan
+input #merupakan angka yang akan diinput
+print #adalah kode untuk membaca suatu program
+(*) #Merupakan Suatu kode perkalian
